7d95ecd65eefc493ced06a7a94b4ccfeb538cc33
Khan <junaidkhan.5a4@gmail.com>
2022-07-29 21:16:39 +0530

    python connection to backend and git ignore

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,13 @@
 import requests
 
 endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"# "http://127.0.0.1:8000/"
 
 # get_response = requests.get(endpoint)
 # print(get_response.text)
 
 get_response = requests.get(endpoint, json = {"query": "Hello World"})
-print(get_response.json()) # see content type and check for data instead of json in request
+# print(get_response.json()) # see content type and check for data instead of json in request
+print(get_response.text)
+print(get_response.status_code)
