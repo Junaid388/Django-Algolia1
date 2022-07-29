c96640e22c823e80f5061efc53aaacd2a26c1494
Khan <junaidkhan.5a4@gmail.com>
2022-07-29 22:38:09 +0530

    Rest Framework View and Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
 import json
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return Response(data)
+
+
+def api_home_model(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
@@ -18,7 +32,7 @@ def api_home(request, *args, **kwargs):
     
     return JsonResponse(data)
 
-def api_home_old(request, *args, **kwargs):
+def api_home_normal(request, *args, **kwargs):
     # request  -> HttpRequest  -> Django
     # request.body
     # print(request.GET)
