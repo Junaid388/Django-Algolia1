dd27e6f225c176976b4aac5e6781db91f2a4bcfc
Khan <junaidkhan.5a4@gmail.com>
2022-07-29 21:51:57 +0530

    Echo response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your first Django API response"})
+    # request  -> HttpRequest  -> Django
+    # request.body
+    # print(request.GET)
+    # print(request.POST)
+    body = request.body # byte string of JSON data
+    # print(body)
+    data = {}
+    try:
+        data = json.loads(body)  # string of Json data -> Python Dict
+    except:
+        pass
+    # print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -7,8 +7,8 @@ endpoint = "http://localhost:8000/api/" # "http://127.0.0.1:8000/"
 # get_response = requests.get(endpoint)
 # print(get_response.text)
 
-get_response = requests.get(endpoint, json = {"query": "Hello World"})
+get_response = requests.get(endpoint, params={"abc":123}, json={"query": "Hello World"})
 # print(get_response.json()) # see content type and check for data instead of json in request
 # print(get_response.text)
 print(get_response.status_code)
-print(get_response.json()['message'])
+print(get_response.json())
